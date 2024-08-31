fb47b17e5f3edef9a794894cb011e53dcce1682a
julian <juliantvi@gmail.com>
2024-08-30 22:57:50 -0500

    Update photos

--- src/components/Collage.js
+++ src/components/Collage.js
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './Collage.css';
 
 const photoList = [
-  { src: `${process.env.PUBLIC_URL}/images/wedding (1).JPG`, text: 'Nos conocimos un día,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (2).JPG`, text: 'sin planes, sin prisas.🌟' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (3).JPG`, text: 'Momentos inesperados,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (4).JPG`, text: 'se convirtieron en magia.✨' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (5).JPG`, text: 'Cada sonrisa, un sueño,🌈' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (6).JPG`, text: 'cada abrazo, una promesa.💖' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (7).JPG`, text: 'Nos amamos intensamente,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (8).JPG`, text: 'sin miedo, sin dudas.💫' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (9).JPG`, text: 'Las pruebas llegaron,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (10).JPG`, text: 'nos hicieron más fuertes.💪' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (11).JPG`, text: 'Supimos quienes eramos,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (12).JPG`, text: 'ella era mi hogar.🏠' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (13).JPG`, text: 'Con un anillo en mano,💍' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (14).JPG`, text: 'le pedí ser mi vida.' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (15).JPG`, text: 'Ella sonrió y dijo sí!😄💍' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (16).JPG`, text: 'ahora juntos caminamos,' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (17).JPG`, text: 'hacia nuestro altar.👰🤵' },
-  { src: `${process.env.PUBLIC_URL}/images/wedding (18).JPG`, text: '¡Y estás Invitado!🎉' }
+  { src: `${process.env.PUBLIC_URL}/images/wedding1.JPG`, text: 'Nos conocimos un día,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding2.JPG`, text: 'sin planes, sin prisas.🌟' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding3.JPG`, text: 'Momentos inesperados,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding4.JPG`, text: 'se convirtieron en magia.✨' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding5.JPG`, text: 'Cada sonrisa, un sueño,🌈' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding6.JPG`, text: 'cada abrazo, una promesa.💖' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding7.JPG`, text: 'Nos amamos intensamente,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding8.JPG`, text: 'sin miedo, sin dudas.💫' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding9.JPG`, text: 'Las pruebas llegaron,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding10.JPG`, text: 'nos hicieron más fuertes.💪' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding11.JPG`, text: 'Supimos quienes eramos,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding12.JPG`, text: 'ella era mi hogar.🏠' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding13.JPG`, text: 'Con un anillo en mano,💍' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding14.JPG`, text: 'le pedí ser mi vida.' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding15.JPG`, text: 'Ella sonrió y dijo sí!😄💍' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding16.JPG`, text: 'ahora juntos caminamos,' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding17.JPG`, text: 'hacia el altar.👰🤵' },
+  { src: `${process.env.PUBLIC_URL}/images/wedding18.JPG`, text: '¡Y estás Invitado!🎉' }
 ]
 
 const Collage = ({ ready }) => {
